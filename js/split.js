0671ef9cb0a739aa92fdd3b5ed4be7d77c5be59c
Kenny <stefan.krainer@bitmovin.com>
2022-05-16 11:39:53 +0200

    ensure details tab does not take over all the content

--- js/split.js
+++ js/split.js
@@ -1,12 +1,19 @@
 /** @type HTMLDivElement */
-var split = document.getElementById('resize-divider');
+const split = document.getElementById('resize-divider');
 /** @type HTMLDivElement */
-var requestTab = document.getElementById('requestList');
+const requestTab = document.getElementById('requestList');
+/** @type HTMLDivElement */
+const detailsTab = document.getElementById('detailView');
 
 const minWidth = 150;
 
 const onMouseMove = function (mouseEv) {
-  requestTab.style.width = Math.max(mouseEv.x, minWidth) + 'px';
+  const containerWidth = requestTab.parentElement.clientWidth;
+  const newRequestWidth = Math.max(mouseEv.x, minWidth);
+  const requestPercentageWidth = (newRequestWidth / containerWidth) * 100;
+  const detailsPercentageWidth = 100 - requestPercentageWidth;
+  requestTab.style.width = requestPercentageWidth + '%';
+  detailsTab.style.maxWidth = detailsPercentageWidth + '%';
 };
 split.onmousedown = function (_mouseEv) {
   document.addEventListener('mousemove', onMouseMove);
